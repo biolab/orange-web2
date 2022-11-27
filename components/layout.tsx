417141497b2a6dd7e61b80a900f93ac16aaa4e33
mitjapotocin <mitja.potocin@gmail.com>
2022-11-27 16:41:49 +0100

    Initial commit

---
+++ components/layout.tsx
@@ -0,0 +1,11 @@
+import Navbar from "./navbar/navbar";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <Navbar />
+      <main>{children}</main>
+      {/* <Footer /> */}
+    </>
+  );
+}
---
+++ components/navbar/navbar.tsx
@@ -0,0 +1,10 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li>Link1</li>
+        <li>Link2</li>
+      </ul>
+    </nav>
+  );
+}
