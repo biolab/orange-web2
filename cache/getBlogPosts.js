29ea6d94e4c1d04432c3a9b8781a251f9551d1b5
mitjapotocin <mitja.potocin@gmail.com>
2022-12-04 16:04:57 +0100

    Blog getStaticProps

--- cache/getBlogPosts.js
+++ cache/getBlogPosts.js
@@ -2,73 +2,74 @@ const fs = require("fs");
 const path = require("path");
 const matter = require("gray-matter");
 
-function getAllMDFromDir(dir, done) {
-  var results = [];
-  fs.readdir(dir, function (err, list) {
-    if (err) return done(err);
-    var i = 0;
-    (function next() {
-      var file = list[i++];
-      if (!file) return done(null, results);
-      file = path.resolve(dir, file);
-      fs.stat(file, function (err, stat) {
-        if (stat && stat.isDirectory()) {
-          getAllMDFromDir(file, function (err, res) {
-            results = results.concat(res);
-            next();
-          });
-        } else if (file.endsWith(".md")) {
-          results.push(file);
-          next();
-        } else {
-          next();
-        }
-      });
-    })();
-  });
+function getAllMdFiles() {
+  function throughDirectory(dir) {
+    fs.readdirSync(dir).forEach((file) => {
+      const absolute = path.join(dir, file);
+      if (fs.statSync(absolute).isDirectory()) {
+        return throughDirectory(absolute);
+      } else if (absolute.endsWith(".md")) {
+        files.push(absolute);
+      }
+    });
+  }
+
+  let files = [];
+  throughDirectory("public/blog");
+  return files;
 }
 
 function getPostsData(files) {
-  const posts = files.map((postPath) => {
+  return files.map((postPath) => {
     const fileContents = fs.readFileSync(postPath, "utf8");
-    let [fileName] = /[^/]*$/.exec(postPath);
-    oldSlug = fileName.replace(/\.md$/, "");
-
-    publicFilePath = postPath.split("/public/")[1].replace(fileName, "");
+    const [fileName] = /[^/]*$/.exec(postPath);
+    const oldSlug = fileName.replace(/\.md$/, "");
+    const publicFilePath = postPath.split("public/")[1].replace(fileName, "");
 
     const {
-      data: { title, draft, longExcerpt, url },
+      data: { title, draft, longExcerpt, url, date },
     } = matter(fileContents);
-
+    console.log(title);
     return {
       fullFilePath: postPath,
       publicFilePath,
       oldSlug,
       draft,
       title,
+      date,
       longExcerpt,
       url: (url || title).replaceAll(" ", "-").toLowerCase(),
     };
   });
-  return JSON.stringify(posts);
 }
 
-getAllMDFromDir("public/blog", function (err, files) {
-  if (err) throw err;
-  const posts = getPostsData(files);
+function getBlogsMetadata() {
+  return getPostsData(getAllMdFiles());
+}
 
-  fileContent = `export const blogPosts: {
+function buildCache() {
+  const fileContent = `export const blogPosts: {
     fullFilePath: string,
     publicFilePath: string,
     oldSlug: string,
     draft: boolean,
+    date: string,
     title: string,
     longExcerpt: string,
     url: string,
-  }[] = ${posts};`;
+  }[] = ${JSON.stringify(getBlogsMetadata())};`;
 
   fs.writeFile("cache/blogPosts.tsx", fileContent, function (err) {
-    if (err) return console.log(err);
+    if (err) {
+      console.log(err);
+      return;
+    }
     console.log("Posts cached.");
   });
-});
+}
+
+buildCache();
+
+module.exports = {
+  getBlogsMetadata,
+};
