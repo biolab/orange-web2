e3f553f32c0039341cf9cf184cc4cc6174e303da
mitjapotocin <mitja.potocin@gmail.com>
2022-12-14 08:08:28 +0100

    Rename Layouts

--- components/Image/Image.tsx
+++ components/Image/Image.tsx
@@ -3,10 +3,10 @@ import NextImage from "next/image";
 // Remove this, if it will be deployed on Vercel
 // This is needed to make NextImage work on GH pages
 // Opt-out of image optimization
-const customLoader = () => {
-  return null;
+const customLoader = ({ src }: { src: string }) => {
+  return src;
 };
 
 export default function Image(props: any) {
-  return <NextImage {...props} />;
+  return <NextImage {...props} loader={customLoader} />;
 }
--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,4 +1,4 @@
-import Navbar from "./Navbar/Navbar";
+import Navbar from "./Navbarr/Navbarr";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
