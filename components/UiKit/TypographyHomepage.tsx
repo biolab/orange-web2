88ba1645c37fe7581c7653c78a73078394cbf6f3
Tea Škvarč <teaskvarc@gmail.com>
2023-03-03 12:27:43 +0100

    HTML and CSS for section. Changed adaptMaxWidth

--- components/UiKit/TypographyHomepage.tsx
+++ components/UiKit/TypographyHomepage.tsx
@@ -1,10 +1,20 @@
 import styled from "styled-components";
+import device from "@styles/utils/breakpoints";
 
 export const Heading1 = styled.h1`
   font-size: 62px;
   line-height: 1.04;
   font-weight: 700;
+  color: #fff;
+
+  @media ${device.M} {
+    font-size: 50px;
+  }
+  @media ${device.S} {
+    font-size: 42px;
+  }
 `;
+
 export const Heading2 = styled.h2`
   font-size: 62px;
   line-height: 1.04;
