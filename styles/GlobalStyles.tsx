ce660b28244a9099dd25e26446cda2bf73816ca1
Tea Škvarč <teaskvarc@gmail.com>
2023-03-21 21:34:25 +0100

    added style for BlogDetail and general style for <code> and <pre>

--- styles/GlobalStyles.tsx
+++ styles/GlobalStyles.tsx
@@ -44,7 +44,7 @@ export const GlobalStyle = createGlobalStyle`
     max-width: 100%;
     height: auto;
   }
-
+ 
   a {
     color: unset;
     text-decoration: none;
@@ -56,6 +56,41 @@ export const GlobalStyle = createGlobalStyle`
 
   strong {
     font-weight: 600;
+  }
+
+  pre {
+    display: block;
+    padding: 9.5px;
+    margin: 0 0 10px;
+    font-size: 13px;
+    line-height: 1.42857143;
+    word-break: break-all;
+    word-wrap: break-word;
+    color: #333;
+    background-color: #f5f5f5;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    overflow-x: auto;
+    word-wrap: normal;
+    
+    code {
+      padding: 0;
+      font-size: inherit;
+      color: inherit;
+      white-space: pre-wrap;
+      background-color: transparent;
+      border-radius: 0;
+    }
+  }
+
+  code {
+    font-family: Menlo,Monaco,Consolas,"Courier New",monospace;
+    padding: 2px 4px;
+    font-size: 90%;
+    color: #c7254e;
+    background-color: #f9f2f4;
+    white-space: nowrap;
+    border-radius: 4px;
   }
 
   ::selection {
