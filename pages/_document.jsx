182d651fd529d8fee058192ed26e26b9ef14cbc0
mitjapotocin <mitja.potocin@gmail.com>
2023-03-27 15:07:54 +0200

    Use next fonts

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -31,11 +31,7 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-          <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;600;700&display=swap" />
-        </Head>
+        <Head></Head>
         <body>
           <Main />
           <NextScript />
