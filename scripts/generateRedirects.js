6694e9c9620bc81ee24e521dc9da28fb7640ed10
Vesna Tanko <tankovesna@hotmail.com>
2023-10-19 14:43:12 +0200

    generateRedirects: Filter old blogs, nicer code

--- scripts/generateRedirects.js
+++ scripts/generateRedirects.js
@@ -2,15 +2,12 @@ import fs from "fs";
 import {getBlogsMetadata} from "./getBlogPosts.js";
 
 function writeRedirects() {
-  const _posts = getBlogsMetadata()
-
-  const redirects = []
-  for (let i = 0; i < _posts.length; i++) {
-    redirects.push({
-        "source": _posts[i].oldUrl.slice(0, -1),
-        "destination": "/blog/" + _posts[i].url
-    });
-  }
+  const redirects = getBlogsMetadata()
+    .filter((post) => (post.oldUrl != ""))
+    .map((post) => ({
+      "source": post.oldUrl.slice(0, -1),
+      "destination": "/blog/" + post.url
+    }))
 
 
   fs.writeFile("redirects.json", JSON.stringify(redirects), function (err) {
