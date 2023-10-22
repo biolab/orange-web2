8537f7663581a6be39ddf723470195e084038254
mitjapotocin <36345162+mitjapotocin@users.noreply.github.com>
2023-10-22 16:09:20 +0000

    Deploying to gh-pages from @ biolab/orange-web2@2ef09e9fdba86f7c1e40f5303647a061a52d69c2 🚀

---
+++ _next/static/vlRVx9HGbWBSiWE6py-hr/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002Fblog","\u002Fblog\u002F[slug]","\u002Fdocs","\u002Fexamples","\u002Ffaq","\u002Fgetting-started","\u002Fhome\u002F[slug]","\u002Fwidget-catalog","\u002Fwidget-catalog\u002F[category]\u002F[widget]","\u002Fworkshops"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
