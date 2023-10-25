67a0d9aa3b19bd78bfbfc54ec76980862461ab67
Vesna Tanko <tankovesna@hotmail.com>
2023-10-25 20:37:53 +0200

    usePostForm: Format file

--- hooks/usePostForm.tsx
+++ hooks/usePostForm.tsx
@@ -1,53 +1,54 @@
 import React from "react";
-import {SubmitHandler} from "react-hook-form";
+import { SubmitHandler } from "react-hook-form";
 
 export const usePostForm = (url: string, extraData = {}) => {
+  const [loading, setLoading] = React.useState(false);
+  const [success, setSuccess] = React.useState(false);
+  const [error, setError] = React.useState(false);
 
-    const [loading, setLoading] = React.useState(false);
-    const [success, setSuccess] = React.useState(false);
-    const [error, setError] = React.useState(false);
+  const onSubmit: SubmitHandler<any> = React.useCallback(
+    async (data) => {
+      setLoading(true);
+      setSuccess(false);
+      setError(false);
 
-    const onSubmit: SubmitHandler<any> = React.useCallback(async (data) => {
-        setLoading(true);
-        setSuccess(false);
-        setError(false);
+      const searchParams = new URLSearchParams({
+        ...data,
+        ...extraData,
+      } as any);
 
-        const searchParams = new URLSearchParams({
-            ...data,
-            ...extraData
-        } as any);
+      try {
+        const response = await fetch(url, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+          },
 
-        try {
-            const response = await fetch(url, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-                },
+          body: searchParams.toString(),
+        });
 
-                body: searchParams.toString(),
-            });
+        const data = await response.json();
 
-            const data = await response.json();
+        setLoading(false);
 
-            setLoading(false);
-
-            if (data.Status === 400) {
-                setError(true);
-                return;
-            }
-
-            setSuccess(true);
-        } catch (error) {
-            setLoading(false);
-            setError(true);
+        if (data.Status === 400) {
+          setError(true);
+          return;
         }
-    }, [url, extraData]);
 
+        setSuccess(true);
+      } catch (error) {
+        setLoading(false);
+        setError(true);
+      }
+    },
+    [url, extraData],
+  );
 
-    return {
-        error,
-        success,
-        loading,
-        onSubmit
-    }
+  return {
+    error,
+    success,
+    loading,
+    onSubmit,
+  };
 };
